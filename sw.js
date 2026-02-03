5e9ea622c7b741ae18f01ebb87a146e1bff2f4f2
Alister Media <51060103+alistermedia10@users.noreply.github.com>
2026-02-03 11:08:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,6 @@
-// Hapus semua kode PWA (Install, Activate, Fetch) sebelumnya
-// Ganti dengan kode Monetag Murni
-
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10558656
-};
-
-self.lary = "";
-
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
+    "zoneId": 10558898
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
